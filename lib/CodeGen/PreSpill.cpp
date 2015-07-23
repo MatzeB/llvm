f074333c30509416e33251a25b256233e455ef48
Matthias Braun <matze@braunis.de>
2015-07-23 15:57:33 -0700

    More prespill foobar

--- lib/CodeGen/PreSpill.cpp
+++ lib/CodeGen/PreSpill.cpp
@@ -13,16 +13,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-// TODO: sort includes
+#include "llvm/CodeGen/LiveIntervalAnalysis.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineLoopInfo.h"
+#include "llvm/CodeGen/machineRegisterInfo.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/CodeGen/RegisterClassInfo.h"
 #include "llvm/Pass.h"
 #include "llvm/PassRegistry.h"
-#include "llvm/CodeGen/MachineFunctionPass.h"
-#include "llvm/CodeGen/LiveIntervalAnalysis.h"
-#include "llvm/CodeGen/MachineLoopInfo.h"
-#include "llvm/CodeGen/RegisterClassInfo.h"
-#include "llvm/CodeGen/Passes.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetRegisterInfo.h"
 #include "llvm/Target/TargetSubtargetInfo.h"
+
 using namespace llvm;
 
 #define DEBUG_TYPE "prespill"
@@ -32,6 +34,18 @@ namespace {
     MachineRegisterInfo *MRI;
     LiveIntervals *LIS;
     RegisterClassInfo RegClassInfo;
+    const TargetRegisterInfo *TRI;
+
+    unsigned *Offsets;
+    unsigned NPressureUnits;
+
+    bool inWorkset(unsigned Reg, const unsigned *WorkSet) const;
+
+    void putIntoWorkset(unsigned Reg, unsigned PSet, unsigned *WorkSet) const;
+
+    void removeFromWorkset(unsigned Reg, unsigned *WorkSet) const;
+
+    void spillInBlock(MachineBasicBlock &MBB);
 
   public:
     static char ID;
@@ -67,13 +81,111 @@ void PreSpill::getAnalysisUsage(AnalysisUsage &AU) const {
   MachineFunctionPass::getAnalysisUsage(AU);
 }
 
+bool PreSpill::inWorkset(unsigned Reg, const unsigned *WorkSet) const {
+  for (unsigned I = 0; I < NPressureUnits; ++I) {
+    if (WorkSet[I] == Reg)
+      return true;
+  }
+  return false;
+}
+
+void PreSpill::putIntoWorkset(unsigned Reg, unsigned PSet,
+                              unsigned *WorkSet) const {
+  unsigned Offset = Offsets[PSet];
+  unsigned EndOffset = Offsets[PSet+1] - Offset;
+
+  for (unsigned I = Offset; I < EndOffset; ++I) {
+    if (WorkSet[I] == 0) {
+      WorkSet[I] = Reg;
+      return;
+    }
+  }
+
+  // Nothing found, we need to spill something
+  unsigned BestCost = 10000;
+  unsigned BestPos = 0;
+  for (unsigned I = Offset; I < EndOffset; ++I) {
+    unsigned SpillCost = 1;
+    if (SpillCost < BestCost) {
+      BestPos = I;
+      BestCost = SpillCost;
+    }
+  }
+
+  // Spill WorkSet[I]
+
+  WorkSet[BestPos] = Reg;
+}
+
+void PreSpill::removeFromWorkset(unsigned Reg, unsigned *WorkSet) const {
+  for (unsigned I = 0; I < NPressureUnits; ++I) {
+    if (WorkSet[I] == Reg)
+      WorkSet[I] = 0;
+  }
+}
+
+void PreSpill::spillInBlock(MachineBasicBlock &MBB) {
+  unsigned *WorkSet = new unsigned[NPressureUnits];
+  // TODO: Determine initial workset state
+
+  for (MachineInstr &MI : MBB) {
+
+    // Check Use Operands
+    for (const MachineOperand &MO : MI.uses()) {
+      if (!MO.isReg())
+        continue;
+      unsigned Reg = MO.getReg();
+      if (Reg == 0)
+        continue;
+
+      if (inWorkset(Reg, WorkSet))
+        continue;
+
+      if (TargetRegisterInfo::isVirtualRegister(Reg)) {
+        const TargetRegisterClass *RC = MRI->getRegClass(Reg);
+        for (const int *Set = TRI->getRegClassPressureSets(RC); *Set != -1;
+             ++Set) {
+          putIntoWorkset(Reg, *Set, WorkSet);
+        }
+      }
+    }
+  }
+
+  delete[] WorkSet;
+}
+
 bool PreSpill::runOnMachineFunction(MachineFunction &MF) {
-  MRI = &MF.getRegInfo();
   LIS = &getAnalysis<LiveIntervals>();
+
+  const TargetSubtargetInfo &STI = MF.getSubtarget();
+  TRI = STI.getRegisterInfo();
+  MRI = &MF.getRegInfo();
 
   RegClassInfo.runOnMachineFunction(MF);
 
-  errs() << "Hello PreSpill from " << MF.getName() << '\n';
+  // Compute number of pressure units necessary for the whole function
+  NPressureUnits = 0;
+  unsigned NPressureSets = TRI->getNumRegPressureSets();
+  Offsets = new unsigned[NPressureSets+1];
+  errs() << "NPressureSets: " << NPressureSets << '\n';
+  for (unsigned I = 0; I < NPressureSets; ++I) {
+    Offsets[I] = NPressureUnits;
+    unsigned Limit = TRI->getRegPressureSetLimit(MF, I);
+    NPressureUnits += Limit;
+    const char *Name = TRI->getRegPressureSetName(I);
+    errs() << " Set " << (Name ? Name : "NoName")
+           << " Limit: " << Limit << '\n';
+  }
+  Offsets[NPressureSets] = NPressureUnits;
+
+  for (MachineBasicBlock &MBB : MF) {
+    spillInBlock(MBB);
+  }
+
+  errs() << "Do Something...\n";
+
+  delete[] Offsets;
+
   return false;
 }
 
